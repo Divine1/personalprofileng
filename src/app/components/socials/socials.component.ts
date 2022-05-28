593424443d3fbabc5bc3667b2e2b13fb390287a7
Divine <dchelladurai@vmware.com>
2022-05-28 08:45:26 +0530

    updated

--- src/app/components/socials/socials.component.ts
+++ src/app/components/socials/socials.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialsComponent implements OnInit {
 
+  socialItems:any = [
+    {
+      title : "stackoverflow",
+      url:"https://stackoverflow.com/users/3673073/divine",
+      filepath: "assets/socials/logo-stackoverflow.svg"
+    },
+    {
+      title : "linkedin",
+      url:"https://www.linkedin.com/in/divine6/",
+      filepath: "assets/socials/LI-In-Bug.png"
+    },
+    {
+      title : "github",
+      url:"https://github.com/Divine1",
+      filepath: "assets/socials/GitHub-Mark-120px-plus.png"
+    },
+    {
+      title : "twitter",
+      url:"https://twitter.com/divine_rw",
+      filepath: "assets/socials/2021Twitterlogo-blue.png"
+    },
+    {
+      title : "youtube",
+      url:"https://www.youtube.com/channel/UC8yRCp-SQLFxtCbkOvWahcA",
+      filepath: "assets/socials/yt_logo_rgb_dark.png"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
