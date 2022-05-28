ebca4f4ee074f837fb2087bfe6b0bcace0a3101b
Divine <dchelladurai@vmware.com>
2022-05-28 08:20:43 +0530

    added socials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { SocialsComponent } from './components/socials/socials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    SocialsComponent
   ],
   imports: [
     BrowserModule,
